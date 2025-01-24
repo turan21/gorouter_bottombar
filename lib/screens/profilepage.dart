d48720ae482cf7bedd683ea65f54e6b7217aec4d
Turan Kutuev <turan.kutuev@onoisoftware.com>
2025-01-24 12:26:38 +0600

    feat: added appbar

--- lib/screens/profilepage.dart
+++ lib/screens/profilepage.dart
@@ -5,6 +5,10 @@ class Profilepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+    );
   }
 }
